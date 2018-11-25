0ffc4c46330eec68650aaba1eb6223f0b3aa662e
Emily Woods <hello@emilywoods.me>
2018-11-25 16:42:31 +0100

    Implement multithreaded server.
    Notes:
    - Channels are a way to communicate between threads.
    - A Box allocates memory on the heap and places x into it.
    - A trait tells the compiler about the functionality a type
    must provide.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,80 @@
-pub struct ThreadPool;
+use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<FnBox + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        // Here we use a channel as a way to function as a queue of jobs
+        // and ``execute`` will send a job from ThreadPool to Worker instances
+        // which will send the job to its thread.
+        // ThreadPool creates channel and holds on to sending side of channel
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender,
+        }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F:  FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
     }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+
+        // need to safely share receiver among all workers => need Arc<Mutex<T>> to share ownership across multiple threads.: 
+        let thread = thread::spawn( move || {
+            loop {
+                //lock - acquires mutex; unwrap - panics on errors; recv - receives Job and block
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job; executing.", id);
+
+                job.call_box();
+            }
+        });
+
+        Worker {
+            id,
+            thread,
+        }
+    }
+
 }
