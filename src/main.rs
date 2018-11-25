850a3ae8ed5a468c0410592a8bb78f55af17d33a
Emily Woods <hello@emilywoods.me>
2018-11-25 13:59:46 +0100

    Return a html response
    Notes:
    - ``flush`` will wait and prevent the program from
    continuing until all bytes are written to the connection.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,8 +16,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new(); 
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n {}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
